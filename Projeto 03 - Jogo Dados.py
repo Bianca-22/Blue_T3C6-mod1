728eab21136a60e6a1700b75e43a0a6be20bafda
Bianca Monteiro Corrêa <83734803+Bianca-22@users.noreply.github.com>
2021-06-29 16:31:29 -0300

    Update Projeto 03 - Jogo Dados.py

--- Projeto 03 - Jogo Dados.py
+++ Projeto 03 - Jogo Dados.py
@@ -1,20 +1,16 @@
 # Importando bibliotecas e suas funções
 from random import randint
-from operator import itemgetter
 from time import sleep 
 
-rodadas = []
 jogador = {}
-empates = []
-
 num = int(input('Quantas rodadas você vai querer jogar? '))
 # O For irá rodar conforme a quantidade de rodadas que o usuário escolher
 for r in range(1,num+1):
-    print('***'*20)
+    print('\n***'*20)
     print(f'{r}° RODADA')
     # Esse For é para a quantidade de jogadores
     for j in range(1,5):
-        print('-<-'*20)
+        print('\n-<-'*20)
         print(f'{j}° JOGADOR')
         dado = randint(1,6)
         # Adicionando ao dicionário 'jogador' a chave (Jogador...) e o valor do dado
@@ -23,29 +19,29 @@ for r in range(1,num+1):
         sleep(1)
         print(f'O dado caiu no número {dado}')
         jogador.copy()
-    sorted(jogador.items(), key=itemgetter(1))
-    # Esse For vai percorrer o dicionário até achar o maior valor
-    for m in [max(jogador.values())]:
-        empate = 0
-        # A variavel key será a chave do dicionário e a variavel val será o valor
-        for key,val in jogador.items():
-            # Se o mesmo valor se repetir então dará empate
-            if val == m:
-                empate += 1
-        # Se empatar então o empate será adicionado a lista 'empates'
-        if empate >= 2:
-            empates.append(empate)
-            print('Empatado')
-        # Senão, então o vencedor será adcionado a lista 'rodadas'
-        else:
-            rodadas.append(key)
-            print(f'O vencedor da {r}° rodada é o', key)
-# O count() é para contar a quantidade de vezes que o valor aparece
-# O len() diz o tamanho dalista q será a quantidade de empates
-print(f'''
-O total de vitórias do Jogador 1 é de: {rodadas.count('Jogador 1')}
-O total de vitórias do Jogador 2 é de: {rodadas.count('Jogador 2')}
-O total de vitórias do Jogador 3 é de: {rodadas.count('Jogador 3')}
-O total de vitórias do Jogador 4 é de: {rodadas.count('Jogador 4')}
-O total de empates é de: {len(empates)}
-''')
+    print('\n---'*20)
+    if jogador['jogador2'] < jogador['jogador1'] > jogador['jogador3'] and jogador['jogador1'] > jogador['jogador4']:
+        situacao = 'jogador1'
+        play1 += 1
+    elif jogador['jogador1'] < jogador['jogador2'] > jogador['jogador3'] and jogador['jogador2'] > jogador['jogador4']:
+        situacao = 'jogador2'
+        play2 += 1
+    elif jogador['jogador1'] < jogador['jogador3'] > jogador['jogador2'] and jogador['jogador3'] > jogador['jogador4']:
+        situacao = 'jogador3'
+        play3 += 1
+    elif jogador['jogador2'] < jogador['jogador4'] > jogador['jogador3'] and jogador['jogador4'] > jogador['jogador1']:
+        situacao = 'jogador4'
+        play4 += 1
+    else:
+        situacao = 'Empate'
+    print(f'O ganhador da rodada é o {situacao}\n')
+if play2 < play1 > play3 and play1 > play4:
+    print(f'        O campeão foi o Jogador 1 com {play1} Vitorias')
+elif play1 < play2 > play3 and play2 > play4:
+    print(f'        O campeão foi o Jogador 1 com {play2} Vitorias')
+elif play1 < play3 > play2 and play3 > play4:
+    print(f'        O campeão foi o Jogador 1 com {play3} Vitorias')
+elif play2 < play4 > play3 and play4 > play1:
+    print(f'        O campeão foi o Jogador 1 com {play4} Vitorias')
+else:
+    print('O resultado terminou em empate.')
